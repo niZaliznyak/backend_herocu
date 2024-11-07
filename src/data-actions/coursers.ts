30e4a9e35a25d8503b90f4d3284e789ec0ad4568
Mykyta Zalizniak <novieasdf@gmail.com>
2024-11-07 16:26:53 +0200

    update DataActionsCourses

--- src/data-actions/coursers.ts
+++ src/data-actions/coursers.ts
@@ -1,24 +1,32 @@
 import { DB } from '../db/db'
-
 class DataActionsCourses {
+  private _getCoursesData() {
+    return DB.courses
+  }
+
   getCourses(search: string) {
+    const courses = this._getCoursesData()
+
     if (!search) {
-      return DB.courses
+      return courses
     }
 
-    return DB.courses.filter(({ name }) => name.includes(search))
+    return courses.filter(({ name }) => name.includes(search))
   }
 
   getCourseById(id: string) {
-    return DB.courses.find((course) => course.id === id)
+    const courses = this._getCoursesData()
+    return courses.find((course) => course.id === id)
   }
 
   createCourse(name: string) {
     if (!name) {
       return
     }
+
     const newCourse = { id: new Date().toString(), name }
-    DB.courses.push(newCourse)
+    this._getCoursesData().push(newCourse)
+
     return newCourse
   }
 }
